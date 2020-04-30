c11e05bbf2efd2f85c5b9591d56887f2ab4e6967
Matheus Gonçalves <ms.goncalves98@gmail.com>
2020-04-30 18:23:57 +0100

    Add Speedtest script

---
+++ Network_SpeedTest/speed.py
@@ -0,0 +1,36 @@
+import speedtest
+
+
+def test():
+    s = speedtest.Speedtest()
+    s.get_servers()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    res = s.results.dict()
+    # Print all information - Test
+    #print(res)
+    return res["download"], res["upload"], res["ping"], res["server"], res["client"]
+
+
+def main():
+    for i in range(1):
+        d, u, p, s, c = test()
+        print('\nResult Test #{}\n'.format(i+1))
+        print('Download: {} Mbps'.format(str(d)[:2]))
+        print('Upload: {} Mbps'.format(str(u)[:2]))
+        print('Latency: {:.0f} ms\n'.format(p))
+        print('Server Information:')
+        print('Local: {}'.format(s["name"]))
+        print('Country: {}'.format(s["country"]))
+        print('Sponsor: {}'.format(s["sponsor"]))
+        print('Host: {}\n'.format(s["host"]))
+        print("Client Information:")
+        print('IP: {}'.format(c["ip"]))
+        print('ISP: {}'.format(c["isp"]))
+        print('ISP Rating: {}\n'.format(c["isprating"]))
+
+
+
+if __name__ == '__main__':
+    main()
