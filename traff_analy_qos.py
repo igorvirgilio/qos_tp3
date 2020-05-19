69969f604d707849d9581bd913215ab45aa7b80d
igorvirgilio <igor.virgilio@gmail.com>
2020-05-19 15:47:03 +0100

    Graph ajustmnets

--- traff_analy_qos.py
+++ traff_analy_qos.py
@@ -21,23 +21,16 @@ class Traff_Analy():
 
 
     def capture(self):
-        captura = NFStreamer(source='./Pcap_Files/webex-matheus.pcapng').to_pandas()
-        #captura = NFStreamer(source='/home/igor/UMinho/MCT/captura/04-29_hang-spo-you-what.pcapng').to_pandas()
+        #captura = NFStreamer(source='./Pcap_Files/webex-matheus.pcapng').to_pandas()
+        captura = NFStreamer(source='/home/igor/UMinho/MCT/captura/04-29_hang-spo-you-what.pcapng').to_pandas()
         #captura = NFStreamer(source='/home/igor/UMinho/MCT/mct_tp3/test.pcap').to_pandas()
         self.data = pd.DataFrame(captura) # converte em dataframe
-        #pd.set_option('display.max_rows',320)
-        print("\nA dimensão do arquivo é: ")
-        print(self.data.shape)
 
-    #def clean_DataFrame():
-    #    self.data.rename(columns={'bidirectional_ip_bytes':'bid_'})
 
     def filtering(self):
         self.cat_name = self.data['category_name'].unique()
         self.app_name = self.data['application_name'].unique()
         self.app_prot = self.data['app_protocol'].unique()
-        
-        
 
         self.data_short = self.data.loc[0:,['src_ip','dst_ip',
                 'bidirectional_ip_bytes','bidirectional_packets',
@@ -47,19 +40,12 @@ class Traff_Analy():
                 #'bidirectional_duration_ms',
                 'app_protocol','application_name','category_name',
                 ]]
-        # filtra por aplicações contidas na lista app_name
-        #data_webex = (self.data_short['app_protocol'].isin(self.app_prot[1]))
-        # filtra por aplicações que contenha a string 'Webex'
-        #data_webex = (self.data_short['application_name'].str.contains('Webex'))
-        
+
         # cria vários um dicionario com todas as tabelas separadas pela list criada em cat_name
         self.df_dict = {elem : pd.DataFrame for elem in self.cat_name}
         for key in self.df_dict.keys():
             self.df_dict[key] = self.data_short[:][self.data_short.category_name == key]
         
-        #print(self.df_dict)
-
-
 
     def statistic(self):
         self.total_bi_bytes = (self.data_short['bidirectional_ip_bytes'].sum())/1000
@@ -83,46 +69,38 @@ class Traff_Analy():
         for x in self.cat_name:
             group_x = data_grouped.get_group(str(self.cat_name[i]))
             group_x_name = group_x.iloc[0]['category_name']
-
                       
             all_bytes = (group_x['bidirectional_ip_bytes'].sum())/1000
             print(all_bytes)
             percent_of = round(((all_bytes/self.total_bi_bytes)*100 ),2)
-            #print(percent_of)
+
             app_per_cat = group_x['application_name'].unique()
             num_app = len(app_per_cat)
             group_x = group_x.sort_values(by='bidirectional_raw_bytes', ascending=False)
 
             # Coletando informação dos principais servidores da categoria
             server_freq = group_x['server_info'].value_counts()[:1].index.tolist()
-            #print (group_x['server_info'])
 
-            #server_freq = list(filter(None, group_x['server_info'][:1])
             server_freq = list(filter(None,group_x['server_info']))
-            #print('\n')
-            
-            #print(server_freq)
 
-            newlist = []
+            df_graphlist = []
             j = 0
             for x in server_freq:
                 if server_freq[j]:
-                    newlist.extend(x.split(","))
+                    df_graphlist.extend(x.split(","))
                     j+=1  
                 else:
                     pass
 
-            if len(newlist) == 0:
-                newlist = ['-']  
+            if len(df_graphlist) == 0:
+                df_graphlist = ['-']  
             
-            for word in newlist[:]:
+            for word in df_graphlist[:]:
                 if word.startswith('*'):
-                    newlist.remove(word)
-            print(newlist[0])
+                    df_graphlist.remove(word)
+            print(df_graphlist[0])
             
-
-            print('\n----')
-            values = [group_x_name, all_bytes, percent_of, app_per_cat, num_app, newlist[0]]
+            values = [group_x_name, all_bytes, percent_of, app_per_cat, num_app, df_graphlist[0]]
             zipped = zip(columns,values)
 
             dici = dict(zipped)
@@ -133,26 +111,20 @@ class Traff_Analy():
         # Faz o append do dicitionário no DataFrame e ordena descendentemente a partir do total de Bytes
         self.df_cat_stat = self.df_cat_stat.append(data_dict, True).sort_values(by='total_bytes', ascending=False)
         
-        print(group_x)
 
     
     def print_table(self):
         i = 0
         
         for x in self.app_prot:
-            #print('\n')
-            #print(x)
+
             filt = (self.data_short['app_protocol'] == x) # Cria filtro p/ imprimir somente linhas que contenham o 'app_prot' x
             data_filt = self.data_short.loc[filt] # criar novo DataFrame com somente dados do app_prot x
-            #print(data_filt['application_name'].loc[0])
  
-
             sum = data_filt['bidirectional_ip_bytes'].sum()
             self.data_dict.update({ self.app_name[i]: sum})
             i += 1  
-            #print('O total de bytes transmitidos é de: '+str(sum)) 
 
-                #i = 0 
         
         '''
             Imprime a table de uma categoria específica ou imprime 
@@ -171,14 +143,29 @@ class Traff_Analy():
         print(self.df_cat_stat)
         print ("Dataframe 1:")
         display(self.df_cat_stat.set_index('categoria'))
-        print ("Dataframe 2:")
-        display(HTML(self.df_cat_stat.to_html()))
+
     
     def plot_graphs(self):
 
-        labels = self.df_cat_stat['categoria'].to_list()
-        sizes = self.df_cat_stat['total_bytes'].to_list()
+        # soma todos os bytes das categorias que possuem valor <= 1
+        sum_of_other = self.df_cat_stat.loc[self.df_cat_stat['(%) do total'] <= 1, 'total_bytes'].sum()
+
+        # Elimina as categorias de pouco relevantes
+        df_graph = self.df_cat_stat.loc[self.df_cat_stat['(%) do total'] >= 1]
+        # Cria um novo Dataframe reduzido com os dados para plotagem do gráfico
+        df_graph = df_graph[['categoria', 'total_bytes']].copy()
         
+        # Adiciona uma linha com a categoria 'Others' e com o valor da soma dos bytes daquelas categorias pouco relevantes
+        df_graph = df_graph.append({'categoria':'Others','total_bytes':sum_of_other}, ignore_index=True)
+        print(df_graph)
+        print ("Dataframe 3:")
+        display(self.df_cat_stat.set_index('categoria'))
+        
+        labels = df_graph['categoria'].to_list()
+        sizes = df_graph['total_bytes'].to_list()
+        
+
+
         # Plot
         plt.pie(sizes, labels=labels, #xplode=0.1, #colors=colors,
         autopct='%1.1f%%', shadow=True, startangle=140)
@@ -188,6 +175,7 @@ class Traff_Analy():
         #plt.tight_layout()
 
         plt.axis('equal')
+        plt.title('TP - QoS / Category Graph')
         plt.show(block=FALSE)
 
     def plot_graphs2(self):
@@ -228,6 +216,7 @@ class Traff_Analy():
     
     def win_table(self):
         win = Tk()
+        win.title('TP - QOS / Full Category Table')
         
         pdtabulate=lambda df:tabulate(self.df_cat_stat.set_index('categoria'),headers='keys',tablefmt='psql')
 
@@ -236,34 +225,34 @@ class Traff_Analy():
         table1 = Label(win, text=pdtabulate(self.df_cat_stat),font=('Consolas', 10), justify=LEFT, anchor='nw').grid(sticky='ewns')    
         #table2 = Label(win, text=self.df_cat_stat['total_bytes']).grid(column=1, row=0)
         win.mainloop()
-   
-    #def main(self):
-    #    #traff = Traff_Analy()
-#
-    #    self.capture()
-    #    self.filtering()
-    #    self.statistic()
-    #    self.print_table()
-    #    #self.plot_graphs_barh()
-    #    self.plot_graphs()
-    #    
-    #    self.win_table()
-    #    #self.plot_graphs2()
 
-def main():
-        traff = Traff_Analy()
+    def main(self):
+        #traff = Traff_Analy()
 
-        traff.capture()
-        traff.filtering()
-        traff.statistic()
-        traff.print_table()
-        #traff.plot_graphs_barh()
-        traff.plot_graphs()
+        self.capture()
+        self.filtering()
+        self.statistic()
+        self.print_table()
+        #self.plot_graphs_barh()
+        self.plot_graphs()
         
-        traff.win_table()
-        #traff.plot_graphs2()
+        self.win_table()
+        #self.plot_graphs2()
+"""
+def main():
+    traff = Traff_Analy()
+
+    traff.capture()
+    traff.filtering()
+    traff.statistic()
+    traff.print_table()
+    #traff.plot_graphs_barh()
+    traff.plot_graphs()
     
+    traff.win_table()
+    #traff.plot_graphs2()
+
 
 if __name__ == '__main__':
     main() 
-
+"""
